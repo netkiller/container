44c8a7f24781c490c611cb08846c2102f74d3c39
neo <netkiller@msn.com>
2023-06-28 14:48:57 +0800

    modified:   docker/docker.py

--- docker/docker.py
+++ docker/docker.py
@@ -1,21 +1,30 @@
 #!/usr/bin/env python3
 import sys
-sys.path.insert(0, '/Users/neo/workspace/devops')
 
-from netkiller.docker import *
-# from environment.experiment import experiment
-# from environment.development import development
-# from environment.production import production
+sys.path.insert(0, "/Users/neo/workspace/devops")
+sys.path.insert(0, "/Users/neo/Github/devops")
 
-from compose.devops import devops
-from compose.demo import demo
-# from libexec.portainer import portainer
-# from compose.devops.zentao import zentao
+try:
+    from netkiller.docker import *
+
+    # from environment.experiment import experiment
+    # from environment.development import development
+    # from environment.production import production
+
+    from compose.devops import devops
+    from compose.demo import demo
+
+    # from libexec.portainer import portainer
+    # from compose.devops.zentao import zentao
+
+except ModuleNotFoundError as err:
+    print("pip install netkiller-devops, %s" % (err))
+
 
 if __name__ == "__main__":
     try:
         docker = Docker()
-        # docker.env({'DOCKER_HOST':'ssh://root@192.168.30.13','COMPOSE_PROJECT_NAME':'experiment'}) 
+        # docker.env({'DOCKER_HOST':'ssh://root@192.168.30.13','COMPOSE_PROJECT_NAME':'experiment'})
         # docker.sysctl({"vm.max_map_count": "262144"})
         # docker.environment(experiment)
         # docker.environment(development)
